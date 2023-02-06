43eab0587797d13b5705a90fa3f2348d26f2f5e4
hansstanley <nusstuhss@gmail.com>
2023-02-06 10:06:39 +0800

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Stack } from 'react-bootstrap';
 import { NAV_LINKS } from '../../utils/constants';
 
 export default function HomePage() {
 	return (
 		<Container fluid className="text-center py-5">
-			<h1 className="display-5 fw-bold">Hire-a-Patient</h1>
-			<p className="lead">Tagline goes here</p>
+			<Stack gap={5}>
+				<h1 className="display-1 fw-bold">Hire-a-Patient</h1>
+				<p className="lead">Tagline goes here</p>
+			</Stack>
 			<Button variant="secondary" href={NAV_LINKS.JOBS}>
 				Explore Jobs
 			</Button>
