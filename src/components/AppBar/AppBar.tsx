2e7abedb4abe4f79f5482d82e18a2f89ba529455
hansstanley <nusstuhss@gmail.com>
2023-02-06 20:31:23 +0800

    Update app bar and error page

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,28 +2,28 @@ import { Container, Dropdown, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { NAV_LINKS } from '../../utils/constants';
 
 export default function AppBar() {
-	return (
-		<Navbar expand="lg">
-			<Container>
-				<Navbar.Brand href="/">Hire-a-Patient</Navbar.Brand>
-				<Navbar.Toggle />
-				<Navbar.Collapse>
-					<Nav className="justify-content-end w-100">
-						<Nav.Link href="#">About</Nav.Link>
-						<Nav.Link href={NAV_LINKS.JOBS}>Jobs</Nav.Link>
-						<Nav.Link href={NAV_LINKS.BLOG}>Blog</Nav.Link>
-						<NavDropdown title="my-username">
-							<NavDropdown.Item href={NAV_LINKS.PROFILE}>
-								Profile
-							</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href={NAV_LINKS.LOGIN}>
-								Sign in
-							</NavDropdown.Item>
-						</NavDropdown>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Hire-a-Patient</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="justify-content-end w-100">
+            <Nav.Link href={NAV_LINKS.ABOUT}>About</Nav.Link>
+            <Nav.Link href={NAV_LINKS.JOBS}>Jobs</Nav.Link>
+            <Nav.Link href={NAV_LINKS.BLOG}>Blog</Nav.Link>
+            <NavDropdown title="my-username">
+              <NavDropdown.Item href={NAV_LINKS.PROFILE}>
+                Profile
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href={NAV_LINKS.LOGIN}>
+                Sign in
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,15 +4,17 @@ import { useRouteError } from 'react-router-dom';
 import { NAV_LINKS } from '../utils/constants';
 
 export default function ErrorPage() {
-	return (
-		<Container className="d-flex mt-5 align-items-center justify-content-center">
-			<Card>
-				<Card.Body>
-					<Card.Title>Oops!</Card.Title>
-					<Card.Text>An unexpected error has occured.</Card.Text>
-					<Button href={NAV_LINKS.HOME}>Return home</Button>
-				</Card.Body>
-			</Card>
-		</Container>
-	);
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: 'calc(100vh - 56px)' }}>
+      <Card>
+        <Card.Body>
+          <Card.Title>Oops!</Card.Title>
+          <Card.Text>An unexpected error has occured.</Card.Text>
+          <Button href={NAV_LINKS.HOME}>Return home</Button>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
