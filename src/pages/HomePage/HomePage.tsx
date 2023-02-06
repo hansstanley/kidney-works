240a97fdd95ef02032cb779e6646e034c36907ea
igezt <igez1537@gmail.com>
2023-02-06 18:47:35 +0800

    centered home screen text

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,19 @@
 import { Button, Container, Stack } from 'react-bootstrap';
 import { NAV_LINKS } from '../../utils/constants';
+import "./HomePage.css";
 
 export default function HomePage() {
 	return (
-		<Container fluid className="text-center py-5">
-			<Stack gap={5}>
-				<h1 className="display-1 fw-bold">Hire-a-Patient</h1>
-				<p className="lead">Tagline goes here</p>
-			</Stack>
-			<Button variant="secondary" href={NAV_LINKS.JOBS}>
-				Explore Jobs
-			</Button>
-		</Container>
+		<div className='homescreen'>
+			<Container fluid className="text-center align-items-center">
+				<Stack gap={5}>
+						<h1 className="display-1 fw-bold">Hire-a-Patient</h1>
+						<p className="lead">Tagline goes here</p>
+				</Stack>
+				<Button variant="secondary" href={NAV_LINKS.JOBS}>
+					Explore Jobs
+				</Button>
+			</Container>
+		</div>
 	);
 }
