98ff40d2588573e5c43a689f65c31e277ed20bfb
hansstanley <nusstuhss@gmail.com>
2023-02-06 21:19:40 +0800

    Change login layout to a card

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,27 +1,23 @@
-import { Button, Col, Container, Row, Stack } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row, Stack } from 'react-bootstrap';
 import PageHero from '../../components/PageHero';
 
 export default function LoginPage() {
-	return (
-		<Container style={{ height: '80vh' }}>
-			<Row className="h-100">
-				<Col xs={8}>
-					<PageHero
-						title="Welcome!"
-						tagline="Sign in with one of the methods"
-						noDivider
-					/>
-				</Col>
-				<Col xs={1}>
-					<div className="vr d-flex h-100" />
-				</Col>
-				<Col>
-					<Stack gap={2} className="justify-content-center h-100">
-						<Button>Sign in with Google</Button>
-						<Button variant="secondary">Sign in with Email</Button>
-					</Stack>
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container
+      className="d-flex flex-column justify-content-center align-content-center"
+      style={{ height: 'calc(100vh - 56px)' }}>
+      <Row>
+        <Col xs={6} className="mx-auto">
+          <Card className="text-center">
+            <Card.Body>
+              <Card.Title>Welcome</Card.Title>
+              <Card.Text>Find a new career today!</Card.Text>
+              <hr />
+              <Button>Sign in with Google</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
