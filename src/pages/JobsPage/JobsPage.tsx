0904207bdb74750b979c92b3234fc5cf81abedbe
hansstanley <nusstuhss@gmail.com>
2023-02-07 09:52:08 +0800

    Implement job search

--- src/pages/JobsPage/JobsPage.tsx
+++ src/pages/JobsPage/JobsPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import {
   Button,
   ButtonGroup,
@@ -16,6 +16,7 @@ import JobFormModal from './JobFormModal';
 export default function JobsPage() {
   const jobs = useLoaderData() as AppJob[];
   const [showForm, setShowForm] = useState(false);
+  const [search, setSearch] = useState('');
 
   const handleShowCreateForm = () => {
     setShowForm(true);
@@ -24,6 +25,27 @@ export default function JobsPage() {
   const handleHideCreateForm = () => {
     setShowForm(false);
   };
+
+  const filterJob = useCallback(
+    (job: AppJob) => {
+      if (!search) {
+        return true;
+      }
+      const lowerSearch = search.toLowerCase();
+      return [
+        job.title,
+        job.description,
+        job.company,
+        job.requirements,
+        job.specialRequirements,
+      ]
+        .map((x) => x?.toLowerCase())
+        .some((x) => x?.includes(lowerSearch));
+    },
+    [search],
+  );
+
+  const visibleJobs = useMemo(() => jobs.filter(filterJob), [jobs, filterJob]);
 
   return (
     <Container className="pb-5">
@@ -37,23 +59,38 @@ export default function JobsPage() {
         </Button>
         <Form>
           <Stack direction="horizontal" gap={2}>
-            <Form.Control type="search" placeholder="Search" className="w-25" />
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="w-25"
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
           </Stack>
         </Form>
-        {jobs.map((j) => (
-          <Card key={j.id}>
+        {visibleJobs.length ? (
+          visibleJobs.map((j) => (
+            <Card key={j.id}>
+              <Card.Body>
+                <Card.Title>{j.title}</Card.Title>
+                <Card.Text>{j.description}</Card.Text>
+                <ButtonGroup>
+                  <Button variant="light" href={`${NAV_LINKS.JOBS}/${j.id}`}>
+                    View
+                  </Button>
+                  <Button>Apply</Button>
+                </ButtonGroup>
+              </Card.Body>
+            </Card>
+          ))
+        ) : (
+          <Card>
             <Card.Body>
-              <Card.Title>{j.title}</Card.Title>
-              <Card.Text>{j.description}</Card.Text>
-              <ButtonGroup>
-                <Button variant="light" href={`${NAV_LINKS.JOBS}/${j.id}`}>
-                  View
-                </Button>
-                <Button>Apply</Button>
-              </ButtonGroup>
+              <Card.Text>No jobs found.</Card.Text>
             </Card.Body>
           </Card>
-        ))}
+        )}
       </Stack>
       <JobFormModal show={showForm} onHide={handleHideCreateForm} />
     </Container>
