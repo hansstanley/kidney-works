41bd402e331fdc5a0cfeb819acfe8d5f513ecbfc
hansstanley <nusstuhss@gmail.com>
2023-02-08 17:33:46 +0800

    Add about and home page tagline

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,17 +2,35 @@ import { Card, Container, Stack } from 'react-bootstrap';
 import PageHero from '../../components/PageHero';
 
 export default function AboutPage() {
-	return (
-		<div>
-            <PageHero title='About' tagline='insert tagline'/>
-			<Container fluid className="text-center align-items-center">
-                <Stack gap={3}>
-                    <p>
-                        Hello! We are a group looking to make a difference in lives of patients with liver complications. Did you know that 90% of people...
-                    </p>
-                </Stack>
-
-			</Container>
-		</div>
-	);
+  return (
+    <div>
+      <Container className="text-center align-items-center">
+        <Stack gap={3}>
+          <PageHero
+            title="About"
+            tagline="Empowering dialysis patients, enriching the workforce"
+          />
+          <Card>
+            <Card.Body>
+              <Card.Text className="text-start">
+                Our job portal is dedicated to connecting employers with kidney
+                dialysis patients, providing a platform for patients to find
+                employment opportunities that are flexible and accommodating to
+                their medical needs. We believe that every individual, including
+                dialysis patients, has the right to work and contribute to
+                society, and our goal is to make this a reality. Our mission is
+                to bridge the gap between employers looking for qualified and
+                motivated employees and patients seeking meaningful employment
+                opportunities. With our platform, employers can access a pool of
+                skilled and dedicated employees, and patients can take control
+                of their careers, financial stability, and quality of life. Join
+                us in creating a more inclusive and equitable job market, where
+                everyone has an equal opportunity to succeed.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,21 @@
 import { Button, Container, Stack } from 'react-bootstrap';
 import { NAV_LINKS } from '../../utils/constants';
-import "./HomePage.css";
+import './HomePage.css';
 
 export default function HomePage() {
-	return (
-		<div className='homescreen'>
-			<Container fluid className="text-center align-items-center">
-				<Stack gap={5}>
-						<h1 className="display-1 fw-bold">Hire-a-Patient</h1>
-						<p className="lead">Tagline goes here</p>
-				</Stack>
-				<Button variant="secondary" href={NAV_LINKS.JOBS}>
-					Explore Jobs
-				</Button>
-			</Container>
-		</div>
-	);
+  return (
+    <div className="homescreen">
+      <Container fluid className="text-center align-items-center">
+        <Stack gap={5}>
+          <h1 className="display-1 fw-bold">Hire-a-Patient</h1>
+          <p className="lead">
+            Empowering dialysis patients, enriching the workforce
+          </p>
+        </Stack>
+        <Button variant="secondary" href={NAV_LINKS.JOBS}>
+          Explore Jobs
+        </Button>
+      </Container>
+    </div>
+  );
 }
