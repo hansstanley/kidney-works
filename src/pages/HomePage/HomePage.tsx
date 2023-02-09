93a48f1e914d0ee28bb1fea2cdfc4e57d9207025
hansstanley <nusstuhss@gmail.com>
2023-02-09 08:36:46 +0800

    Add react spring

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,20 @@
+import './HomePage.css';
 import { Button, Container, Stack } from 'react-bootstrap';
 import { NAV_LINKS } from '../../utils/constants';
-import './HomePage.css';
+import { animated, useSpring } from '@react-spring/web';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+  const props = useSpring({
+    from: { backgroundColor: '#ffffff' },
+    to: { backgroundColor: '#ffffff00' },
+    delay: 500,
+    config: { duration: 2000 },
+  });
+
   return (
-    <div className="homescreen">
+    <animated.div style={props} className="homescreen">
       <Container fluid className="text-center align-items-center">
         <Stack gap={5}>
           <h1 className="display-1 fw-bold">Hire-a-Patient</h1>
@@ -12,10 +22,10 @@ export default function HomePage() {
             Empowering dialysis patients, enriching the workforce
           </p>
         </Stack>
-        <Button variant="primary" href={NAV_LINKS.JOBS}>
+        <Button variant="primary" onClick={() => navigate(NAV_LINKS.JOBS)}>
           Explore Jobs
         </Button>
       </Container>
-    </div>
+    </animated.div>
   );
 }
