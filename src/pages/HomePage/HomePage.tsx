028cd5f13c3e7cc0e4a70135361863528c024749
hansstanley <nusstuhss@gmail.com>
2023-02-09 15:22:00 +0800

    Refactor HomeFeature from HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,19 @@
 import './HomePage.css';
-import { Button, Card, Col, Container, Row, Stack } from 'react-bootstrap';
+import { Button, Container, Stack } from 'react-bootstrap';
 import { NAV_LINKS } from '../../utils/constants';
 import { useNavigate } from 'react-router-dom';
 import Page from '../../components/Page';
 import AnimatedBackground from '../../components/AnimatedBackground';
 import { useState } from 'react';
 import { animated, useSpring } from '@react-spring/web';
+import HomeFeature from './HomeFeature';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const AnimatedButton = animated(Button);
 
 export default function HomePage() {
   const navigate = useNavigate();
   const [clicked, setClicked] = useState(false);
-  const [exploreClicked, setExploreClicked] = useState(false);
 
   const { x } = useSpring({
     from: { x: 0 },
@@ -20,29 +21,26 @@ export default function HomePage() {
     config: { duration: 1000 },
   });
 
-  const exploreSpring = useSpring({
-    from: { scale: 1 },
-    to: { scale: exploreClicked ? 0 : 1 },
-  });
+  const [exploreSpring, exploreApi] = useSpring(() => ({ from: { scale: 1 } }));
 
-  const circleSpring = useSpring({
+  const [circleSpring, circleApi] = useSpring(() => ({
     from: { width: '0vw', height: '0vh', borderRadius: '50vw', opacity: 1 },
-    to: exploreClicked
-      ? {
-          width: '100vw',
-          height: '100vh',
-          borderRadius: '0',
-          opacity: 0,
-        }
-      : { width: '0vw', height: '0vh', borderRadius: '50vw', opacity: 1 },
-  });
+  }));
 
   const handleClick = () => {
     setClicked(!clicked);
   };
 
   const handleExploreClick = () => {
-    setExploreClicked(true);
+    exploreApi.start({ to: { scale: 0 } });
+    circleApi.start({
+      to: {
+        width: '100vw',
+        height: '100vh',
+        borderRadius: '0',
+        opacity: 0,
+      },
+    });
     setTimeout(() => navigate(NAV_LINKS.JOBS), 500);
   };
 
@@ -73,88 +71,14 @@ export default function HomePage() {
               onClick={handleExploreClick}>
               Explore Jobs
             </AnimatedButton>
-            <animated.div
-              className="bg-primary position-absolute top-50 start-50 translate-middle"
-              style={circleSpring}
-            />
           </Container>
         </div>
-        <Page.Body noCard>
-          <Row>
-            <Col>
-              <Card>
-                <Card.Header>Empower</Card.Header>
-                <Card.Body>
-                  <Card.Subtitle>
-                    Platform to share patients' experiences, challenges, and
-                    triumphs
-                  </Card.Subtitle>
-                  <hr />
-                  <Card.Text>
-                    Empowering patients to share their stories can help build a
-                    supportive community where they can connect with others who
-                    are facing similar challenges. These stories can serve as a
-                    source of inspiration and encouragement for others, helping
-                    to build resilience and hope.
-                    <br />
-                    <br />
-                    The blog can also serve as an educational resource for
-                    people who are interested in learning more about life with
-                    kidney disease and dialysis, increasing awareness and
-                    understanding of the disease and the experiences of those
-                    who live with it.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <Card.Header>Connect</Card.Header>
-                <Card.Body>
-                  <Card.Subtitle>
-                    Bridging the gap between employers and patients
-                  </Card.Subtitle>
-                  <hr />
-                  <Card.Text>
-                    By offering a platform that is specifically tailored to the
-                    needs of kidney dialysis patients, Hire-a-Patient can help
-                    patients overcome the challenges they may face in the job
-                    search process, such as limited mobility, scheduling
-                    conflicts, and medical restrictions.
-                    <br />
-                    <br />
-                    They can also obtain resources and support to help them
-                    succeed in the workplace, such as information on
-                    accommodations and flexible work arrangements.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <Card.Header>Apply</Card.Header>
-                <Card.Body>
-                  <Card.Subtitle>More manageable, less stressful</Card.Subtitle>
-                  <hr />
-                  <Card.Text>
-                    Hire-a-Patient offers job listings from employers who
-                    understand the challenges and limitations faced by dialysis
-                    patients, and are willing to make necessary accommodations.
-                    This means that job seekers can apply for positions with
-                    confidence, knowing that their medical needs will be taken
-                    into consideration.
-                    <br />
-                    <br />
-                    By leveraging the power of technology and connecting with a
-                    supportive community, dialysis patients can find meaningful
-                    employment and take control of their careers.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Page.Body>
+        <HomeFeature />
       </Page>
+      <animated.div
+        className="bg-primary position-fixed top-50 start-50 translate-middle"
+        style={circleSpring}
+      />
     </AnimatedBackground>
   );
 }
